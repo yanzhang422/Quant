b665cf0e2b8c7cf7a62bb8e01f5bc007735f2716
Bonnie.zy <32243023+yanzhang81@users.noreply.github.com>
2020-10-29 15:08:25 +0100

    First Commit

---
+++ Trading.py
@@ -0,0 +1,10 @@
+Course schedule:
+Lec 1: Automatic Trading Introduction
+Lec 2: Quant Trading System
+Lec 3: Build Quant DB
+Lec 4: Finance Data Analysis 
+Lec 5: Stratedy Modeling I
+Lec 6: Stratedy Modeling with ML
+Lec 7: Model Evaluation and Risk Control
+Lec 8: Automatic Trading Building
+Lec 9: Quant Strategy
